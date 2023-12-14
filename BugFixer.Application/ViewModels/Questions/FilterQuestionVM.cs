17e0ed7084b1e8d4dc8de957320c0a5fb460175a
Alireza <alirezaghafari0101@gmail.com>
2023-12-14 19:51:10 +0330

    add partial and vm to filter questions

---
+++ BugFixer.Application/ViewModels/Questions/FilterQuestionVM.cs
@@ -0,0 +1,14 @@
+﻿using BugFixer.Application.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugFixer.Application.ViewModels.Questions
+{
+    public class FilterQuestionVM: BasePaging<QuestionVM>
+    {
+        public string OrderByType { get; set; }
+    }
+}
